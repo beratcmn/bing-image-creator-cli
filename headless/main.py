0882a7d9568c7c7ead17712c8eeefc04024edc2b
beratcmn <beratcmn@gmail.com>
2023-04-13 03:35:29 +0300

    Main program cycle

--- headless/main.py
+++ headless/main.py
@@ -1,11 +1,21 @@
 from pyvda import AppView, VirtualDesktop, get_apps_by_z_order, get_virtual_desktops
-from selenium import webdriver
 
 from DesktopManager import DesktopManager
+from WebScapper import Scrapper
 
 
 def main():
     desktopManager = DesktopManager()
+    scrapper = Scrapper()
+
+    new_desktop = desktopManager.create_desktop()
+
+    scrapper.open_browser()
+
+    browser_window = AppView(scrapper.browserWindowHWND)
+    desktopManager.move_window(browser_window, new_desktop)
+
+    scrapper.start()
 
 
 if __name__ == "__main__":
